2d98ffa88ff81f6cf9e65e5927869613fc87ec38
Xaakkeep <xaakkeep@gmail.com>
2023-03-27 15:08:34 +0300

    Add Programs

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿string[] array = { "Tom", "Sam", "Bob", "Tomas", "Samuel", "Alexander", "if", "Tokio", "in", "123", "12345", ">_<", "XD" };
+Console.Write($"Задан массив: ");
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Вывод массива длина строк которых меньше или ровна 3:");
+PrintArray(array);
+InputArray(array);
+
+
+void InputArray(string[] arr)
+{
+    Console.Write(" => {");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            Console.Write($"{arr[i]}, ");
+        }
+
+    }
+    Console.Write("}");
+}
+
+void PrintArray(string[] arr)
+{
+    Console.Write("{");
+    Console.Write(String.Join(", ", arr));
+    Console.Write("}");
+}
